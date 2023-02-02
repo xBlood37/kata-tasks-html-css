8bc046c58d683c74cc35ec3a35487b90cb84022c
xBlood37 <123d.123b@mail.ru>
2023-02-02 21:29:46 +0300

    ref

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 window.addEventListener("DOMContentLoaded", () => {
   const btn = document.querySelector("#create-account");
-  btn.addEventListener("click", () => alert("Register"));
+  btn.addEventListener("click", () =>
+    alert(
+      "Пришли как-то Пупа и Лупа получать зарплату. Но в бухгалтерии всё перепутали и Лупа получил зарплату за Пупу, а Пупа за Лупу."
+    )
+  );
 });
