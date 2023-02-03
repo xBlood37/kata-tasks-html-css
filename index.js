50e51229cc7354b55b5767a8c1d6493a88950926
xBlood37 <123d.123b@mail.ru>
2023-02-03 22:33:44 +0300

    js css refactor

--- index.js
+++ index.js
@@ -1,8 +1,30 @@
 window.addEventListener("DOMContentLoaded", () => {
   const btn = document.querySelector("#create-account");
+  const facebookButton = document.querySelector(".facebook");
+  const linkedinButton = document.querySelector(".linkedin");
+
   btn.addEventListener("click", () =>
     alert(
       "Пришли как-то Пупа и Лупа получать зарплату. Но в бухгалтерии всё перепутали и Лупа получил зарплату за Пупу, а Пупа за Лупу."
     )
+  );
+
+  function moveAnimationClick(btn) {
+    const btnContains = btn.classList.contains("animation");
+    if (btnContains) {
+      btn.classList.remove("animation");
+      return;
+    }
+    if (!btnContains) {
+      btn.classList.add("animation");
+      return;
+    }
+  }
+
+  facebookButton.addEventListener("click", () =>
+    moveAnimationClick(facebookButton)
+  );
+  linkedinButton.addEventListener("click", () =>
+    moveAnimationClick(linkedinButton)
   );
 });
